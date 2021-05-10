c2ea471ffb8f05bc54a45dea8c58d9a5f84dd432
NadiasinaNico <nadiasinanicodev@gmail.com>
2021-05-11 00:45:48 +0300

    renfote porfolio

--- main.js
+++ main.js
@@ -5,7 +5,32 @@ $(document).ready(function() {
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings : ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed : 100,
+        backSpeed: 60,
+        loop : true
+    });
+    var typed = new Typed(".typing-2", {
+        strings : ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed : 100,
+        backSpeed: 60,
+        loop : true
+    });
+
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
