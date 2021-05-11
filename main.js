049282aa74b66c275d247bc8602f2e32ffb5c7f6
NadiasinaNico <nadiasinanicodev@gmail.com>
2021-05-11 09:21:06 +0300

    fix error

--- main.js
+++ main.js
@@ -35,25 +35,28 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
 
-    })
+    // owl carousel script
+
+    $('.carousel').owlCarousel({
+        // margin: 20,
+        // loop: true,
+        // autoplayTimeOut: 2000,
+        // autoplayHoverPause: true,
+        // responsive: {
+        //     0:{
+        //         items: 1,
+        //         nav: false
+        //     },
+        //     600:{
+        //         items: 2,
+        //         nav: false
+        //     },
+        //     1000:{
+        //         items: 3,
+        //         nav: false
+        //     }
+        // }
+
+    });
 });
