d8ef396a06dab3f362e80bf60eaf1c822129bff7
Nadiasina588 <nadiasinanicodev@gmail.com>
2021-05-21 02:05:37 +0300

    refonte du porfolio

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ $(document).ready(function() {
 
     // typing animation script
     var typed = new Typed(".typing", {
-        strings : ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings : ["Developer",  "Designer", "Freelancer"],
         typeSpeed : 100,
         backSpeed: 60,
         loop : true
     });
     var typed = new Typed(".typing-2", {
-        strings : ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings : [ "Developer",  "Designer", "Freelancer"],
         typeSpeed : 100,
         backSpeed: 60,
         loop : true
